d18a0173cb6cc97cab81c92fdfb2b6fa6dd5f147
Nabeel Siddiqui <nabeelsiddiqui@Nabeels-Air.fios-router.home>
2020-01-08 10:09:54 -0500

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,29 @@
 // your Bomb code here!
+import React, { Component } from 'react'
+
+export default class Bomb extends Component {
+
+    state = {
+        secondsLeft: this.props.initialCount
+    }
+
+    renderBoom = () => {
+        if (this.state.secondsLeft === 0) {
+            return "Boom!"
+        } else {
+            return `${this.state.secondsLeft} seconds left before I go boom!`
+            
+        }
+    }
+    
+
+
+
+    render() {
+        return (
+            <div>
+                {this.renderBoom()}
+            </div>
+        )
+    }
+}
